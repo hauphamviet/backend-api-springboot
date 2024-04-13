32ac3f2380b1d67da3c14fbc569a82f66eace9a9
Hau <pvhau2002@gmail.com>
2024-04-13 21:35:49 +0700

    update securiry

--- src/main/java/com/thuctaptotnghiem/thuctaptotnghiep/controller/AuthController.java
+++ src/main/java/com/thuctaptotnghiem/thuctaptotnghiep/controller/AuthController.java
@@ -19,16 +19,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/auth/")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class AuthController {
 
     private final UserService userService;
